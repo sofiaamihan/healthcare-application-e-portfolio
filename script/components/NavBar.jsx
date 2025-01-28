e32d469c0f114e9c1f19f0639badd2012be2bd04
sofiaamihan <sofiaamihanmrespeto@gmail.com>
2025-01-28 14:17:07 +0800

    Design NavBar

--- script/components/NavBar.jsx
+++ script/components/NavBar.jsx
@@ -1,16 +1,19 @@
 import React, { useEffect } from "react";
 import { NavLink, Link, useLocation } from "react-router-dom";
-import { Layout } from 'antd';
+import { Layout, Row } from 'antd';
+import '../../css/NavBar.css';
 
 export default function NavBar(){
     const location = useLocation();
     return(
         <Layout>
             <div className="nav-bar">
-                <NavLink className="nav-link" to="/about">About</NavLink>
-                <NavLink className="nav-link" to="/progress">Progress</NavLink>
-                <NavLink className="nav-link" to="/prototype">Prototype</NavLink>
-                <NavLink className="nav-link" to="/summary">Summary</NavLink>
+                <Row>
+                    <NavLink className="nav-link" to="/about">About</NavLink>
+                    <NavLink className="nav-link" to="/progress">Progress</NavLink>
+                    <NavLink className="nav-link" to="/prototype">Prototype</NavLink>
+                    <NavLink className="nav-link" to="/summary">Summary</NavLink>
+                </Row>
             </div>
         </Layout>
     )
--- script/main.jsx
+++ script/main.jsx
@@ -3,7 +3,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ConfigProvider, theme } from 'antd';
 import App from './App.jsx';
-import '../css/index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
