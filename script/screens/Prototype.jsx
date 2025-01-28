722f80696fbd574f1839200aafb4a0bce475fb17
sofiaamihan <sofiaamihanmrespeto@gmail.com>
2025-01-28 16:00:23 +0800

    Create Prototype section base layout

--- script/screens/Prototype.jsx
+++ script/screens/Prototype.jsx
@@ -1,14 +1,86 @@
-import React from 'react';
-import { Layout } from 'antd';
-import { useNavigate } from 'react-router-dom';
-const { Content } = Layout;
+import React, {useState} from 'react';
+import { Layout, Menu } from 'antd';
+const { Content, Sider } = Layout;
 
 export default function Prototype(){
-    const navigate = useNavigate();
+    const [selectedKey, setSelectedKey] = useState('1');
+        
+        function getItem(label, key) {
+            return {
+              key,
+              label,
+            };
+          }
+    
+        const items = [
+            getItem('Interactive High-Fi Prototype', '1'),
+            getItem('Project Proposal', '2'),
+            getItem('Self-Directed Learning Report', '3'),
+            getItem('Test Report', '4'),
+            getItem('Powerpoint Presentation Slides', '5'),
+            getItem('Final Working Application', '6'),
+        ]
+    
+        const handleMenuClick = (e) => {
+            setSelectedKey(e.key);
+        };
+    
+        const renderContent = () => {
+            switch (selectedKey) {
+                case '1':
+                    return <div>Uno</div>;
+                case '2':
+                    return <div>Dos</div>;
+                case '3':
+                    return <div>Tres</div>;
+                case '4':
+                    return <div>Apat</div>;
+                case '5':
+                    return <div>Lima</div>;
+                case '6':
+                    return <div>6</div>;
+                default:
+                    return <div>Select a tab to see content</div>;
+            }
+        };
+
+    // TODO - Make the full label seen
 
     return(
         <Layout className='layout'>
-            <h1>Prototype</h1>
+            <Content className='context-box' style={{
+                padding: '64px',
+            }}>
+                <Layout style={{
+                    padding: '24px',
+                    background: "#D0DDD0",
+                    borderRadius: 24,
+                    height: '70vh'
+                }}>
+                    <Sider style={{
+                        background: "#FFFFFF",
+                        borderRadius: 16,
+                    }}>
+                        <Menu 
+                            defaultSelectedKeys={['1']} 
+                            selectedKeys={[selectedKey]} 
+                            onClick={handleMenuClick} 
+                            items={items} style={{
+                                padding:'16px 0px',
+                                background: "#FFFFFF",
+                                borderRadius: 16,
+                        }}/>
+                    </Sider>
+                    <Content
+                        style={{
+                        padding: '0 24px',
+                        minHeight: 280,
+                        }}
+                    >
+                        {renderContent()}
+                    </Content>
+                </Layout>
+            </Content>
         </Layout>
     )
 };
