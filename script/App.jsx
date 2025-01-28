088e3e94c70f5d0f91b7c746ef769ed0efc0cf42
sofiaamihan <sofiaamihanmrespeto@gmail.com>
2025-01-28 11:37:14 +0800

    Create Routes

--- script/App.jsx
+++ script/App.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import { Route, createBrowserRouter, RouterProvider, createRoutesFromElements } from "react-router-dom";
 
+const About = lazy(() => import('./screens/About.jsx'));
+const Progress = lazy(() => import('./screens/Progress.jsx'));
+const Prototype = lazy(() => import('./screens/Prototype.jsx'));
+const Summary = lazy(() => import('./screens/Summary.jsx'));
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<BaseLayout />}>
+        <Route path="about" element={<About />} />
+        <Route path="progress" element={<Progress />} />
+        <Route path="prototype" element={<Prototype />} />
+        <Route path="summary" element={<Summary />} />
     </Route>
   )
 );
