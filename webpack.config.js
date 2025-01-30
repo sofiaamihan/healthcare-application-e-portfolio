fc4178e6fc72e1b533db888b2a4dfdcaa8ab0199
sofiaamihan <sofiaamihanmrespeto@gmail.com>
2025-01-30 13:57:53 +0800

    Configure GitHub Pages

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'), // Output - Dynamically generated filenames 
         filename: '[name].[contenthash].js',
+        publicPath: '/healthcare-application-e-portfolio/',
         clean: true,
     },
     module : {
