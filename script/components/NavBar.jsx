8872ebae53a66122627d2b4d134261f2a9556cc6
sofiaamihan <sofiaamihanmrespeto@gmail.com>
2025-01-30 11:28:13 +0800

    Add Logo to NavBar

--- script/components/NavBar.jsx
+++ script/components/NavBar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { NavLink, Link, useLocation } from "react-router-dom";
 import { Layout, Row } from 'antd';
 import '../../css/NavBar.css';
+import logo from '../../public/logo.png';
 
 export default function NavBar(){
     const location = useLocation();
@@ -11,6 +12,9 @@ export default function NavBar(){
                 <Row>
                     <NavLink className="nav-link" to="/about">About</NavLink>
                     <NavLink className="nav-link" to="/progress">Progress</NavLink>
+                    <NavLink className="nav-link" to="/">
+                        <img src={logo} alt="Logo" width="44" height="44"></img>
+                    </NavLink>
                     <NavLink className="nav-link" to="/prototype">Prototype</NavLink>
                     <NavLink className="nav-link" to="/summary">Summary</NavLink>
                 </Row>
