c178f8f3e25cd27c5f4d9975f74cc86adb7252ad
sofiaamihan <sofiaamihanmrespeto@gmail.com>
2025-02-01 17:28:14 +0800

    Deployment Updates

--- dist/339.2487ce3233647fb0e0fb.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkhealthcare_application_e_portfolio=self.webpackChunkhealthcare_application_e_portfolio||[]).push([[339],{339:(e,a,t)=>{t.r(a),t.d(a,{default:()=>n});t(6540);var l=t(5448),r=t(7767),c=t(4848);l.A.Content;function n(){(0,r.Zp)();return(0,c.jsx)(l.A,{className:"layout",children:(0,c.jsx)("h1",{children:"Summary"})})}}}]);
-//# sourceMappingURL=339.2487ce3233647fb0e0fb.js.map
---
+++ dist/892.c2ecc68da75562f33247.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkhealthcare_application_e_portfolio=self.webpackChunkhealthcare_application_e_portfolio||[]).push([[892],{892:(a,c,e)=>{e.r(c),e.d(c,{default:()=>l});var r=e(5448);e(6540);const n=e.p+"images/background.png";e.p;var s=e(4848);function l(){return(0,s.jsx)(r.A,{className:"layout",children:(0,s.jsx)("div",{className:"background-container",children:(0,s.jsx)("img",{src:n,alt:"background",className:"background"})})})}}}]);
+//# sourceMappingURL=892.c2ecc68da75562f33247.js.map
