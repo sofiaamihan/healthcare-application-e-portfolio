3bc41c8907e21630a0230a32331a24b78dfeb875
Huimei <huhuimei@tp.edu.sg>
2025-02-11 20:27:59 +0800

    Updates

---
+++ 107.75ea65f8f1f2e2e149dd.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkhealthcare_application_e_portfolio=self.webpackChunkhealthcare_application_e_portfolio||[]).push([[107],{2107:(a,c,e)=>{e.r(c),e.d(c,{default:()=>l});var r=e(5448);e(6540);const n=e.p+"images/background.png";var s=e(4848);function l(){return(0,s.jsx)(r.A,{className:"layout",children:(0,s.jsx)("div",{className:"background-container",children:(0,s.jsx)("img",{src:n,alt:"background",className:"background"})})})}}}]);
+//# sourceMappingURL=107.75ea65f8f1f2e2e149dd.js.map
